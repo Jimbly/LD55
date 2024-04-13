dd15db54f841a3d0b6b471dc98b358211bf947f6
Jimb Esser <wasteland@gmail.com>
2024-04-13 07:49:09 -0700

    Modeless magic circle editing

--- src/client/main.ts
+++ src/client/main.ts
@@ -3,6 +3,7 @@
 const local_storage = require('glov/client/local_storage');
 local_storage.setStoragePrefix('ld55'); // Before requiring anything else that might load from this
 
+import assert from 'assert';
 import * as effects from 'glov/client/effects';
 import { effectsQueue } from 'glov/client/effects';
 import * as engine from 'glov/client/engine';
@@ -11,6 +12,10 @@ import { framebufferEnd } from 'glov/client/framebuffer';
 import {
   KEYS,
   inputClick,
+  inputDrag,
+  inputTouchMode,
+  keyDownEdge,
+  longPress,
   mouseOver,
   mousePos,
 } from 'glov/client/input';
@@ -23,17 +28,16 @@ import { spriteSetGet } from 'glov/client/sprite_sets';
 // } from 'glov/client/sprites';
 import {
   LINE_CAP_ROUND,
-  buttonText,
   drawCircle,
   drawLine,
   playUISound,
   scaleSizes,
   setButtonHeight,
   setFontHeight,
-  uiButtonHeight,
+  uiTextHeight,
 } from 'glov/client/ui';
-import { DataObject } from 'glov/common/types';
-import { ridx } from 'glov/common/util';
+import { DataObject, VoidFunc } from 'glov/common/types';
+import { nop, ridx } from 'glov/common/util';
 import {
   Vec4,
   v2dist,
@@ -64,7 +68,7 @@ const palette = [
   vec4(1, 1, 1, 1),
   vec4(0, 1, 0.2, 1),
   vec4(0, 0, 0, 1),
-  vec4(1, 0, 0, 1),
+  vec4(1, 0.25, 0.25, 1),
   vec4(0, 0, 1, 1),
   vec4(196/255, 17/255, 255/255, 1),
 ];
@@ -82,27 +86,29 @@ const style_eval = fontStyle(null, {
   glow_inner: 0,
   glow_outer: 2.5,
 });
+const style_help = style_eval;
 
 const MC_X0 = 560;
-const MC_Y0 = 155;
+const MC_Y0 = 120;
 const MC_W = 800;
 const MC_R = MC_W / 2;
 const MC_XC = MC_X0 + MC_W/2;
 const MC_YC = MC_Y0 + MC_W/2;
 const LINE_W = 8;
+const LINE_W_DELETE = 4;
 const POWER_R = 50;
 
 const CIRCLE_STEPS = 12;
 const ANGLE_STEPS = 48;
 const CIRCLE_MIN = 4;
-type Mode = 'line' | 'circle' | 'power';
+// type Mode = 'line' | 'circle' | 'power';
 type EvalType = 'components' | 'ink';
 type Evaluation = Record<EvalType, number>;
 class GameState {
   circles: number[] = [8];
   lines: [number, number, number, number][] = [[0, 5, 0, 20]];
   power: [number, number][] = [[0, 15]];
-  mode: Mode = 'line';
+  // mode: Mode = 'line';
   placing: null | [number, number] = null;
   constructor() {
     if (engine.DEBUG) {
@@ -236,11 +242,11 @@ function queuePostprocess(): void {
   effectsQueue(Z.POSTPROCESS + 1, doColorEffect);
 }
 
-const MODES: [Mode, string][] = [
-  ['circle', 'O'],
-  ['line', '/'],
-  ['power', 'P'],
-];
+// const MODES: [Mode, string][] = [
+//   ['circle', 'O'],
+//   ['line', '/'],
+//   ['power', 'P'],
+// ];
 
 const EVALS: [EvalType, string][] = [
   ['components', 'Components'],
@@ -250,31 +256,32 @@ const EVALS: [EvalType, string][] = [
 const EVAL_W = 200;
 const PAD = 8;
 let mouse_pos = vec2();
+let was_drag = false;
 function statePlay(dt: number): void {
   gl.clearColor(palette[PALETTE_BG][0], palette[PALETTE_BG][1], palette[PALETTE_BG][2], 0);
   queuePostprocess();
-  let { circles, lines, power, mode, placing } = game_state;
+  let { circles, lines, power, placing } = game_state;
 
-  let button_height = uiButtonHeight();
-  let xx = (game_width - MODES.length * (button_height + PAD) - PAD) / 2;
-  for (let ii = 0; ii < MODES.length; ++ii) {
-    let pair = MODES[ii];
-    if (buttonText({
-      x: xx,
-      y: game_height - button_height,
-      w: button_height,
-      h: button_height,
-      text: pair[1],
-      disabled: mode === pair[0],
-      hotkey: KEYS['1'] + ii,
-    })) {
-      mode = game_state.mode = pair[0];
-      game_state.placing = null;
-    }
-    xx += button_height + PAD;
-  }
+  // let button_height = uiButtonHeight();
+  // let xx = (game_width - MODES.length * (button_height + PAD) - PAD) / 2;
+  // for (let ii = 0; ii < MODES.length; ++ii) {
+  //   let pair = MODES[ii];
+  //   if (buttonText({
+  //     x: xx,
+  //     y: game_height - button_height,
+  //     w: button_height,
+  //     h: button_height,
+  //     text: pair[1],
+  //     disabled: mode === pair[0],
+  //     hotkey: KEYS['1'] + ii,
+  //   })) {
+  //     mode = game_state.mode = pair[0];
+  //     game_state.placing = null;
+  //   }
+  //   xx += button_height + PAD;
+  // }
 
-  xx = (game_width - EVALS.length * (EVAL_W + PAD) - PAD) / 2;
+  let xx = (game_width - EVALS.length * (EVAL_W + PAD) - PAD) / 2;
   for (let ii = 0; ii < EVALS.length; ++ii) {
     let pair = EVALS[ii];
     let v = game_state.evaluation[pair[0]];
@@ -295,11 +302,27 @@ function statePlay(dt: number): void {
     peek: true,
   });
   mousePos(mouse_pos);
+  let drag;
+  if (do_hover) {
+    drag = inputDrag({
+      min_dist: MC_R / CIRCLE_STEPS,
+    });
+  }
+
+  const CIRCLE_INTERACT_DIST = MC_R / CIRCLE_STEPS / 2;
 
   let center_cursor_dist = v2dist(mouse_pos, [MC_XC, MC_YC]);
   let cursor_angle = round(atan2(mouse_pos[0] - MC_XC, mouse_pos[1] - MC_YC) * ANGLE_STEPS / (2 * PI));
   let cursor_circle = -1;
   let cursor_circle_dist = Infinity;
+  let drag_start_center_cursor_dist = 0;
+  let drag_start_angle = 0;
+  let drag_start_circle = -1;
+  let drag_start_circle_dist = Infinity;
+  if (drag) {
+    drag_start_center_cursor_dist = v2dist(drag.start_pos, [MC_XC, MC_YC]);
+    drag_start_angle = round(atan2(drag.start_pos[0] - MC_XC, drag.start_pos[1] - MC_YC) * ANGLE_STEPS / (2 * PI));
+  }
   for (let ii = 0; ii < circles.length; ++ii) {
     let r = circles[ii] / CIRCLE_STEPS * MC_R;
     drawCircleAA(MC_XC, MC_YC, Z.CIRCLES, r, LINE_W, 1, palette[PALETTE_CIRCLE]);
@@ -309,153 +332,14 @@ function statePlay(dt: number): void {
       cursor_circle_dist = circle_cursor_dist;
       cursor_circle = ii;
     }
-  }
-  if (mode === 'circle' && do_hover) {
-    if (cursor_circle !== -1 && cursor_circle_dist < MC_R/CIRCLE_STEPS/2) {
-      // near existing
-      drawCircleAA(MC_XC, MC_YC, Z.HOVER, circles[cursor_circle] / CIRCLE_STEPS * MC_R,
-        LINE_W / 2, 1, palette[PALETTE_HOVER_DELETE]);
-      if (inputClick()) {
-        playUISound('button_click');
-        let tail = circles.length - 1;
-        for (let ii = lines.length - 1; ii >= 0; --ii) {
-          let line = lines[ii];
-          if (line[0] === cursor_circle || line[2] === cursor_circle) {
-            lines.splice(ii, 1);
-          } else {
-            if (line[0] === tail) {
-              line[0] = cursor_circle;
-            }
-            if (line[2] === tail) {
-              line[2] = cursor_circle;
-            }
-          }
-        }
-        for (let ii = power.length - 1; ii >= 0; --ii) {
-          if (power[ii][0] === cursor_circle) {
-            power.splice(ii, 1);
-          } else if (power[ii][0] === tail) {
-            power[ii][0] = cursor_circle;
-          }
-        }
-        ridx(circles, cursor_circle);
-        game_state.commit();
-      }
-    } else {
-      let circle_r = round(center_cursor_dist / MC_R * CIRCLE_STEPS);
-      if (circle_r > CIRCLE_STEPS && circle_r < CIRCLE_STEPS + 4) {
-        circle_r = CIRCLE_STEPS;
-      }
-      if (circle_r >= CIRCLE_MIN && circle_r <= CIRCLE_STEPS) {
-        drawCircleAA(MC_XC, MC_YC, Z.HOVER, circle_r / CIRCLE_STEPS * MC_R,
-          LINE_W / 2, 1, palette[PALETTE_HOVER]);
-        if (inputClick()) {
-          playUISound('button_click');
-          circles.push(circle_r);
-          game_state.commit();
-        }
-      }
-    }
-  }
-  if (mode === 'line' && do_hover) {
-    if (placing) {
-      let [sx, sy] = circAngleToXY(placing[0], placing[1]);
-      drawCircle(sx, sy, Z.HOVER, LINE_W * 2, 0.5, palette[PALETTE_HOVER]);
-    }
-    if (cursor_circle_dist < MC_R / 2) {
-      // do rollover
-      let line: [number, number, number, number] = [0,0,0,0];
-      let valid = true;
-      if (placing) {
-        line = [placing[0], placing[1], cursor_circle, cursor_angle];
-        if (line[0] > line[2] || line[0] === line[2] && line[1] > line[3]) {
-          line = [cursor_circle, cursor_angle, placing[0], placing[1]];
-        }
-        if (cursor_circle === placing[0] && angleDiff(cursor_angle, placing[1]) < 5) {
-          valid = false;
-        }
-      }
-      let [tx, ty] = circAngleToXY(cursor_circle, cursor_angle);
-      if (valid && inputClick()) {
-        playUISound('button_click');
-        if (placing) {
-          if (cursor_circle === placing[0] && cursor_angle === placing[1]) {
-            placing = game_state.placing = null;
-          } else {
-            let removed = false;
-            for (let ii = 0; ii < lines.length; ++ii) {
-              if (v4same(lines[ii], line)) {
-                lines.splice(ii, 1);
-                removed = true;
-                break;
-              }
-            }
-            if (!removed) {
-              lines.push(line);
-            }
-            game_state.commit();
-            placing = game_state.placing = null;
-          }
-        } else {
-          placing = game_state.placing = [cursor_circle, cursor_angle];
-        }
-      } else {
-        drawCircle(tx, ty, Z.HOVER, LINE_W * 2, 0.5, palette[valid ? PALETTE_HOVER : PALETTE_HOVER_DELETE]);
-        if (placing) {
-          let [sx, sy] = circAngleToXY(placing[0], placing[1]);
-          let would_remove = false;
-          for (let ii = 0; ii < lines.length; ++ii) {
-            if (v4same(lines[ii], line)) {
-              would_remove = true;
-              break;
-            }
-          }
-          drawLine(sx, sy, tx, ty, Z.HOVER, LINE_W / 2, 0.5,
-            palette[(would_remove || !valid) ? PALETTE_HOVER_DELETE : PALETTE_HOVER]);
-        }
-      }
-    }
-  }
-  if (mode === 'power' && do_hover) {
-    if (cursor_circle_dist < MC_R / 2) {
-      // do rollover
-      let screen_pos = circAngleToXY(cursor_circle, cursor_angle);
-      let [tx, ty] = screen_pos;
-      let pos: [number, number] = [cursor_circle, cursor_angle];
-      let valid = true;
-      for (let ii = 0; ii < power.length; ++ii) {
-        let test = power[ii];
-        let opos = circAngleToXY(test[0], test[1]);
-        if (v2distSq(screen_pos, opos) < POWER_R * 2 * POWER_R * 2 && (
-          screen_pos[0] !== opos[0] || screen_pos[1] !== opos[1]
-        )) {
-          valid = false;
-        }
-      }
-      if (valid && inputClick()) {
-        playUISound('button_click');
-        let removed = false;
-        for (let ii = 0; ii < power.length; ++ii) {
-          if (v2same(power[ii], pos)) {
-            power.splice(ii, 1);
-            removed = true;
-            break;
-          }
-        }
-        if (!removed) {
-          power.push(pos);
-        }
-        game_state.commit();
-      } else {
-        let would_remove = false;
-        for (let ii = 0; ii < power.length; ++ii) {
-          if (v2same(power[ii], pos)) {
-            would_remove = true;
-            break;
-          }
-        }
-        drawCircle(tx, ty, Z.HOVER, POWER_R * 0.75, 0.5,
-          palette[(would_remove || !valid) ? PALETTE_HOVER_DELETE : PALETTE_HOVER]);
+
+    if (drag) {
+      let drag_start_circle_cursor_dist = abs(drag_start_center_cursor_dist - r);
+      if (drag_start_circle_cursor_dist < drag_start_circle_dist &&
+        drag_start_circle_cursor_dist < CIRCLE_INTERACT_DIST
+      ) {
+        drag_start_circle_dist = drag_start_circle_cursor_dist;
+        drag_start_circle = ii;
       }
     }
   }
@@ -470,6 +354,248 @@ function statePlay(dt: number): void {
     let [x, y] = circAngleToXY(pow[0], pow[1]);
     drawCircle(x, y, Z.POWER, POWER_R, 1, palette[PALETTE_BG]);
     drawCircleAA(x, y, Z.POWER + 0.1, POWER_R, LINE_W, 1, palette[PALETTE_POWER]);
+  }
+
+  let right_click: [string, VoidFunc] | null = null;
+  let left_click: [string, VoidFunc] | null = null;
+  let drag_start: [string, VoidFunc] | null = null;
+  let drag_stop: [string, VoidFunc] | null = null;
+  if (do_hover && !drag) {
+    // CIRCLE logic
+    if (cursor_circle !== -1 && cursor_circle_dist < CIRCLE_INTERACT_DIST) {
+      // near existing
+      // drawCircleAA(MC_XC, MC_YC, Z.HOVER, circles[cursor_circle] / CIRCLE_STEPS * MC_R,
+      //   LINE_W_DELETE, 1, palette[PALETTE_HOVER_DELETE]);
+      right_click = [
+        'Erase circle',
+        function () {
+          let tail = circles.length - 1;
+          for (let ii = lines.length - 1; ii >= 0; --ii) {
+            let line = lines[ii];
+            if (line[0] === cursor_circle || line[2] === cursor_circle) {
+              lines.splice(ii, 1);
+            } else {
+              if (line[0] === tail) {
+                line[0] = cursor_circle;
+              }
+              if (line[2] === tail) {
+                line[2] = cursor_circle;
+              }
+            }
+          }
+          for (let ii = power.length - 1; ii >= 0; --ii) {
+            if (power[ii][0] === cursor_circle) {
+              power.splice(ii, 1);
+            } else if (power[ii][0] === tail) {
+              power[ii][0] = cursor_circle;
+            }
+          }
+          ridx(circles, cursor_circle);
+        },
+      ];
+    } else {
+      let circle_r = round(center_cursor_dist / MC_R * CIRCLE_STEPS);
+      if (circle_r > CIRCLE_STEPS && circle_r < CIRCLE_STEPS + 4) {
+        circle_r = CIRCLE_STEPS;
+      }
+      if (circle_r >= CIRCLE_MIN && circle_r <= CIRCLE_STEPS && !circles.includes(circle_r)) {
+        drawCircleAA(MC_XC, MC_YC, Z.HOVER, circle_r / CIRCLE_STEPS * MC_R,
+          LINE_W / 2, 1, palette[PALETTE_HOVER]);
+        left_click = [
+          'Draw circle',
+          function () {
+            circles.push(circle_r);
+          },
+        ];
+      }
+    }
+  }
+  if (do_hover && drag_start_circle === -1) {
+    // POWER logic
+    if (cursor_circle_dist < CIRCLE_INTERACT_DIST) {
+      // do rollover
+      let screen_pos = circAngleToXY(cursor_circle, cursor_angle);
+      let [tx, ty] = screen_pos;
+      let pos: [number, number] = [cursor_circle, cursor_angle];
+      let valid = true;
+      for (let ii = 0; ii < power.length; ++ii) {
+        let test = power[ii];
+        let opos = circAngleToXY(test[0], test[1]);
+        if (v2distSq(screen_pos, opos) < POWER_R * 2 * POWER_R * 2 && (
+          screen_pos[0] !== opos[0] || screen_pos[1] !== opos[1]
+        )) {
+          valid = false;
+        }
+      }
+      let would_remove = false;
+      for (let ii = 0; ii < power.length; ++ii) {
+        if (v2same(power[ii], pos)) {
+          would_remove = true;
+          break;
+        }
+      }
+      if (would_remove) {
+        drawCircle(tx, ty, Z.HOVER, POWER_R * 0.75, 0.5,
+          palette[(would_remove || !valid) ? PALETTE_HOVER_DELETE : PALETTE_HOVER]);
+      }
+      if (valid) {
+        left_click = [
+          would_remove ? 'Erase Power Node' : 'Draw Power Node',
+          function () {
+            let removed = false;
+            for (let ii = 0; ii < power.length; ++ii) {
+              if (v2same(power[ii], pos)) {
+                power.splice(ii, 1);
+                removed = true;
+                break;
+              }
+            }
+            if (!removed) {
+              power.push(pos);
+            }
+          },
+        ];
+      }
+    }
+  }
+  if (do_hover) {
+    // LINE logic
+    if (drag_start_circle !== -1) {
+      placing = game_state.placing = [drag_start_circle, drag_start_angle];
+      let [sx, sy] = circAngleToXY(drag_start_circle, drag_start_angle);
+      drawCircle(sx, sy, Z.HOVER, LINE_W * 2, 0.5, palette[PALETTE_HOVER]);
+    }
+    if (cursor_circle_dist < CIRCLE_INTERACT_DIST) {
+      // do rollover
+      let line: [number, number, number, number] = [0,0,0,0];
+      let valid = true;
+      if (placing) {
+        line = [placing[0], placing[1], cursor_circle, cursor_angle];
+        if (line[0] > line[2] || line[0] === line[2] && line[1] > line[3]) {
+          line = [cursor_circle, cursor_angle, placing[0], placing[1]];
+        }
+        if (cursor_circle === placing[0] && angleDiff(cursor_angle, placing[1]) < 5) {
+          valid = false;
+        }
+      }
+      let [tx, ty] = circAngleToXY(cursor_circle, cursor_angle);
+      if (valid) {
+        drawCircle(tx, ty, Z.HOVER, LINE_W * 2, 0.5, palette[PALETTE_HOVER]);
+        if (placing) {
+          let [sx, sy] = circAngleToXY(placing[0], placing[1]);
+          let would_remove = false;
+          for (let ii = 0; ii < lines.length; ++ii) {
+            if (v4same(lines[ii], line)) {
+              would_remove = true;
+              break;
+            }
+          }
+          drawLine(sx, sy, tx, ty, Z.HOVER, LINE_W_DELETE, 0.5,
+            palette[would_remove ? PALETTE_HOVER_DELETE : PALETTE_HOVER]);
+        }
+
+        if (placing) {
+          let would_remove = false;
+          for (let ii = 0; ii < lines.length; ++ii) {
+            if (v4same(lines[ii], line)) {
+              would_remove = true;
+              break;
+            }
+          }
+          drag_stop = [
+            would_remove ? 'Erase line' : 'Draw line',
+            function () {
+              let removed = false;
+              for (let ii = 0; ii < lines.length; ++ii) {
+                if (v4same(lines[ii], line)) {
+                  lines.splice(ii, 1);
+                  removed = true;
+                  break;
+                }
+              }
+              if (!removed) {
+                lines.push(line);
+              }
+              placing = game_state.placing = null;
+            },
+          ];
+        } else {
+          drag_start = [
+            'Draw/Erase line',
+            nop,
+          ];
+        }
+      } else {
+        assert(placing);
+        drawCircle(tx, ty, Z.HOVER, LINE_W * 2, 0.5, palette[PALETTE_HOVER_DELETE]);
+        let [sx, sy] = circAngleToXY(placing[0], placing[1]);
+        drawLine(sx, sy, tx, ty, Z.HOVER, LINE_W_DELETE, 0.5,
+          palette[PALETTE_HOVER_DELETE]);
+        drag_stop = [
+          'Draw line (too short)',
+          nop,
+        ];
+      }
+    } else if (placing) {
+      drag_stop = [
+        'Draw line',
+        nop,
+      ];
+    }
+  }
+
+  let text_height = uiTextHeight();
+  if (left_click) {
+    font.draw({
+      style: style_help,
+      x: game_width / 2,
+      y: game_height - (text_height + PAD) * 3,
+      align: ALIGN.HCENTER,
+      text: `Left-click: ${left_click[0]}`,
+    });
+    if (inputClick({ button: 0 })) {
+      playUISound('button_click');
+      left_click[1]();
+      game_state.commit();
+    }
+  }
+  if (drag_start || drag_stop) {
+    font.draw({
+      style: style_help,
+      x: game_width / 2,
+      y: game_height - (text_height + PAD) * 2,
+      align: ALIGN.HCENTER,
+      text: `Drag: ${drag_start ? drag_start[0] : drag_stop![0]}`,
+    });
+    if (drag && !was_drag && drag_start) {
+      // playUISound('button_click');
+      drag_start[1]();
+      // game_state.commit();
+    }
+    if (was_drag && !drag && drag_stop) {
+      playUISound('button_click');
+      drag_stop[1]();
+      game_state.commit();
+    }
+  }
+  if (right_click) {
+    font.draw({
+      style: style_help,
+      x: game_width / 2,
+      y: game_height - (text_height + PAD),
+      align: ALIGN.HCENTER,
+      text: `Right-click: ${right_click[0]}`,
+    });
+    if (inputClick({ button: 2 }) || inputTouchMode() && longPress({ min_time: 1000 }) || keyDownEdge(KEYS.DEL)) {
+      playUISound('button_click');
+      right_click[1]();
+      game_state.commit();
+    }
+  }
+
+  was_drag = Boolean(drag);
+  if (!drag) {
+    game_state.placing = null;
   }
 }
 
